460a4491eb86ad2acadf981ad626a2c1364aa408
v1lling <sascha.villing@web.de>
2023-01-21 00:04:14 +0100

    Add kula scraper

--- src/event.py
+++ src/event.py
@@ -1,13 +1,15 @@
 import json
 
 class Event:
-    def __init__(self, title, date, time_string, location, city):
+    def __init__(self, title, date, time_string, location, city, pictureUrl, tenant):
         self.eventId = title
         self.eventTitle = title
         self.eventDate = date
         self.eventTimeString = time_string
         self.eventLocation = location
-        self.city = city
+        self.eventCity = city
+        self.eventPictureUrl = pictureUrl
+        self.city = tenant
 
     def pretty_print(self):
         print("Title: %s, Date: %s, Timestring: %s, Location: %s, City: %s" %(self.eventTitle, self.eventDate, self.eventTimeString, self.eventLocation, self.city))
